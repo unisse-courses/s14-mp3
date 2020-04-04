e33f7d1ef3075d4dbdde319b0a038e120b1b92bf
JohannGon118 <johann_gonzales@dlsu.edu.ph>
2020-04-04 11:09:08 +0800

    Added Mongoose for users

--- models/users.js
+++ models/users.js
@@ -40,3 +40,32 @@
     module.exports = mongoose.model('Author', AuthorSchema);
     
 */
+
+const mongoose = require('mongoose');
+
+const databaseURL = 'mongodb://localhost:27017/foodiesdb';
+
+const options = { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false 
+};
+
+
+mongoose.connect(databaseURL, options);
+
+const AuthorSchema = new mongoose.Schema(
+    {
+        firstname:  {type: String, required: true, max: 100},
+        lastname:   {type: String, required: true, max: 100},
+        username:   {type: String, required: true, max: 100},
+        password:   {type: String, required: true, max: 100},
+        bio:        {type: String, required: false, max: 100},
+
+    },
+    {
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true }
+    }
+    );
+
+module.exports = mongoose.model('reciPeeps', AuthorSchema);
